c37e5724111b5bcb6546468daa6b1eb81151fcc7
Muhammad Mujtaba Rehman <73633822+mujee7@users.noreply.github.com>
2022-03-31 19:25:28 +0500

    Add files via upload

---
+++ helper.js
@@ -0,0 +1,10 @@
+export const ETHER_ADDRESS="0x0000000000000000000000000000000000000000"
+export const EVM_REVERT="VM Exception while processing transaction: revert"
+
+export const tokens=(n)=>{
+    return new web3.utils.BN(
+        web3.utils.toWei(n.toString(),"ether")
+    )
+
+}
+export const ether=(n)=>tokens(n);
